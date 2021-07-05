ea45790272b8d030d0519846d14e23f23af0d3cd
itss10 <t1l.g2.o11g@gmail.com>
2021-07-06 04:06:28 +0900

    remove 'console.log'

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -19,7 +19,6 @@ export const UserManagement: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { onSelectUser, selectedUser } = useSelectUser();
   const { loginUser } = useLoginUser();
-  console.log(loginUser);
 
   useEffect(() => getUsers(), []);
 
